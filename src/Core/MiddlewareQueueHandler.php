1365023ede957f8e962296fceebd434198596a35
Oliver Etchebarne <yo@drmad.org>
2021-10-27 00:36:49 -0500

    Add initial implementation for PSR-7 middleware

---
+++ src/Core/MiddlewareQueueHandler.php
@@ -0,0 +1,39 @@
+<?php
+namespace Vendimia\Core;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Vendimia\ObjectManager\ObjectManager;
+
+class MiddlewareQueueHandler implements RequestHandlerInterface
+{
+    private $queue = [];
+    private $fallbackHandler;
+
+    public function __construct(
+        private ObjectManager $object
+    )
+    {
+
+    }
+
+    public function setQueue(array $queue)
+    {
+        $this->queue = $queue;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        if (0 === count($this->queue)) {
+            // FIXME: Nunca debería llegar acá
+            return new \Vendimia\Http\Response;
+        }
+
+        $middleware_class = array_shift($this->queue);
+        $middleware = $this->object->new($middleware_class);
+
+        return $middleware->process($request, $this);
+    }
+}
