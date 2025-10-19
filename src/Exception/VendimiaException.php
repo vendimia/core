d71f8da6235663944afa4dd8cd1f1532efbdd921
Oliver Etchebarne <yo@drmad.org>
2025-10-19 13:50:01 -0500

    Updates Vendimia\Exception\VendimiaException constructor signature to matching \Exception's

--- src/Exception/VendimiaException.php
+++ src/Exception/VendimiaException.php
@@ -1,16 +1,26 @@
 <?php
+
 namespace Vendimia\Exception;
 
 use Exception;
+use Throwable;
 
+/**
+ * Exception class for most of Vendimia-thrown exceptions. It has an extra
+ * 'extra' parameter.
+ */
 class VendimiaException extends Exception
 {
-    private $extra = [];
+    public function __construct(
+        string $message = "",
+        int $code = 0,
+        ?Throwable $previous = null,
 
-    public function __construct($message = '', ...$extra)
+        /** Extra information for this exception */
+        protected array $extra = [],
+    )
     {
-        parent::__construct($message);
-        $this->extra = $extra;
+        parent::__construct($message, $code, $previous);
     }
 
     /**
